2303b9f3793ed5a19a743f7eeef577eca0effe79
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-19 07:10:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// your code here
+function myFun(){
+    let nm = document.getElementById("name");
+    let yr = document.getElementById("year");
+    let url = document.getElementById("url");
+
+    if(nm.value.length!=0 && yr.value.length!=0)
+        url.textContent = `https://localhost:8080/?name=${nm.value}&year=${yr.value}`;
+    else if(nm.value.length!=0)
+        url.textContent = `https://localhost:8080/?name=${nm.value}`;
+    else if(yr.value.length!=0)
+        url.textContent = `https://localhost:8080/?year=${yr.value}`;
+
+    // console.log(typeof nm.value,typeof yr.value);
+}
